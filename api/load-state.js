3a02fb6af92414e2e579fbd19a4c38ca23627afe
inpinkan <yoshipanno2@gmail.com>
2026-06-14 10:14:56 +0900

    Update load-state.js

--- api/load-state.js
+++ api/load-state.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       });
     }
 
-    const key = `pncwallet:${String(walletAddress).toLowerCase()}`;
+    const key = `pnc:wallet:${String(walletAddress).toLowerCase()}`;
 
     const kvRes = await fetch(
       `${url}/get/${encodeURIComponent(key)}`,
